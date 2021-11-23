042adb52655195d2d8bad9564893bab8e155cb9c
Jepmoltho <jept@itu.dk>
2021-11-23 11:52:46 +0100

    saving changes. No progress

--- src/components/pages/AddDuties.js
+++ src/components/pages/AddDuties.js
@@ -5,9 +5,46 @@ import SimpleTextField from "../InputTextRow";
 import ItemCard from "../ItemCard";
 import IconButtons from "../IconButtons";
 import TextButton from "../TextButton";
+import Parse from "parse";
 
 function AddDuties(props) {
   const duties = ["Duty 1", "Duty 2"];
+
+  const Duty = Parse.Object.extend("Duty");
+  const duty = new Duty();
+  //const DutyPointer = duty.set("excursionID", <YE46CEmnds>);
+
+  //duty.set("excursionID", "YE46CEmnds");
+  duty.set("title", "empty dishwasher");
+
+  async function addDuty(e) {
+    e.preventDefault();
+    console.log("Prevented default");
+
+    try {
+      const savedDuty = await duty.save();
+      alert("success");
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  // async function addDuty(e) {
+  //   e.preventDefault();
+  //   try {
+  //     //Create new duty instance
+  //     const newDuty = new Parse.Object("Duty");
+  //     //Defining attributes for duty object
+  //     newDuty.set("title", "testtest");
+  //     await newDuty.save();
+  //   } catch (error) {
+  //     console.log("Could not save duty to database:");
+  //   }
+  // }
+
+  // duty.set("date", Date.now());
+  // const savedDuty = await duty.save();
+
   return (
     <>
       <div className="page-container">
@@ -15,7 +52,7 @@ function AddDuties(props) {
         <div className="card-container-add-duties">
           <Card>
             <div className="card-textfields-container">
-              <SimpleTextField title="Test">
+              <SimpleTextField title="Duty name">
                 <IconButtons add />
               </SimpleTextField>
 
@@ -38,7 +75,12 @@ function AddDuties(props) {
           doneSteps={1}
         />
       </div>
-      <TextButton label="Next" className="green-button-right" link="/shopping-list" />
+      <TextButton
+        label="Next"
+        className="green-button-right"
+        //link="/shopping-list"
+        handleClick={addDuty}
+      />
     </>
   );
 }
